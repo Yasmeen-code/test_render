5ef562ada11083cad51c345bfcae971113023fc2
Yasmeen <yasmeen@example.com>
2026-01-31 20:18:39 +0200

    first commit

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,34 +2,24 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    /**
-     * The path to your "home" route for your application.
-     *
-     * This is used by Laravel authentication to redirect users after login.
-     *
-     * @var string
-     */
-    public const HOME = '/home';
-
-    /**
-     * Define your route model bindings, pattern filters, etc.
-     */
     public function boot(): void
     {
-        $this->routes(function () {
-            // Web routes
+        // Web routes
+        if (file_exists(base_path('routes/web.php'))) {
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+        }
 
-            // API routes
+        // API routes
+        if (file_exists(base_path('routes/api.php'))) {
             Route::prefix('api')
                 ->middleware('api')
                 ->group(base_path('routes/api.php'));
-        });
+        }
     }
 }
