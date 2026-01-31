db63beb77663c1e4a2552a232e1b2f1605822ed6
Yasmeen <yasmeen@example.com>
2026-01-31 20:05:23 +0200

    first commit

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/test', function () {
+    return response()->json([
+        'status' => 'success',
+        'message' => 'API is working!',
+        'time' => now()
+    ]);
+});
